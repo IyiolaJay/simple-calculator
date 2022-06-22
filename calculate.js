f92031ee850b4d73d757c08e8a1131c3ee7212c7
unknown <jimoh.iyiola11@gmail.com>
2022-06-22 09:19:16 -0600

    Initial commit

---
+++ calculate.js
@@ -0,0 +1,27 @@
+function addition(num1, num2) {
+  ans = num1 + num2;
+  console.log(ans);
+}
+
+function subtraction(num1, num2) {
+  ans = num1 - num2;
+  console.log(ans);
+}
+
+function multiplication(num1, num2) {
+  ans = num1 * num2;
+  console.log(ans);
+}
+
+function division(num1, num2) {
+  ans = num1 / num2;
+  console.log(ans);
+}
+
+let num1 = 10;
+let num2 = 4;
+
+addition(num1, num2);
+subtraction(num1, num2);
+division(num1, num2);
+multiplication(num1, num2);
